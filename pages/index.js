6640a3cc237158c0cc3773d8423c11a2e242a3f0
lerontonge <lerontonge@gmail.com>
2023-07-24 07:23:09 -0400

    to be coninued

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { introText, secondaryText } from '../public/introText';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -27,16 +30,14 @@ export default function Home({ allPostsData }) {
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li
-              className={utilStyles.listItem}
-              key={id}
-            >
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          
           ))}
         </ul>
       </section>
